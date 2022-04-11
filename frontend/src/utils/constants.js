a608937feda8085a8db85a36c65bc64475de0fdb
Musmaxim <77632944+Musmaxim@users.noreply.github.com>
2022-04-11 22:18:58 +0300

    :проектная работа № 15.16

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -28,11 +28,3 @@ export const profileSelectors = {
     avatarSelector: ".info__photo",
     avatarEditSelector: ".info__photo-edit",
 };
-
-export const apiConfig = {
-    baseUrl: "https://mesto.nomoreparties.co/v1/cohort-30/",
-    headers: {
-        authorization: "f527135c-0312-442d-818d-6dc714cef781",
-        "Content-Type": "application/json",
-    },
-};
