3e7d8e5b5f266d00be19ba93c6d22b8621657626
Musmaxim <77632944+Musmaxim@users.noreply.github.com>
2022-05-18 20:05:56 +0300

    fix:еще чуть-чуть 1.5

--- backend/app.js
+++ backend/app.js
@@ -16,7 +16,7 @@ const {
   createUser,
 } = require('./controllers/users');
 
-const { PORT = 3001 } = process.env;
+const { PORT = 3000 } = process.env;
 const app = express();
 
 app.use(cookieParser());
